e551caa1cfdc06ccbaacea07f10fd093f88242b6
Гой Антон <a.goy@corp.mail.ru>
2017-11-20 10:11:23 +0300

    Depend only on basic gym

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from distutils.core import setup
 setup(
     name='DQM',
     version='0.0.1',
-    install_requires=['numpy', 'torchvision', 'gym[all]'],
+    install_requires=['numpy', 'torchvision', 'gym'],
     packages=['dqn']
 )
